a64b26923f77f43b2736b3442dbc0d8f79fc142a
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-11-15 13:26:46 +0530

    redicator

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,12 @@
 echo -e "\e[33m Copy MongoDB Repo File \e[0m"
-cp /home/centos/prroboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp /home/centos/prroboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
 echo -e "\e33m Installing MongoDB Server \e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m update MongoDB Listen Address \[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf  &>>/tmp/roboshop.log
 
 echo -e "\e[33m Start MongoDB Service \[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod  &>>/tmp/roboshop.log
+systemctl restart mongod  &>>/tmp/roboshop.log
