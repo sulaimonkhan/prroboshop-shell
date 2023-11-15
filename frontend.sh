85d1977ea35e56ce6767e9398dd6ab537a6e7587
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-11-15 13:30:05 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 echo -e "\e[33m Installing nginx server\e[0m"
-yum install nginx -y
+yum install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Removing Old App Content \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download Frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33m Extaract the frontend content \e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33m update frontend configuration\e[0m"
-cp /home/centos/prroboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf 
+cp /home/centos/prroboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m Starting Nginx server \e[0m"
-systemctl enable nginx 
-systemctl restart nginx 
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
