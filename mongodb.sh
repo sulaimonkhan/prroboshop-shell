5f8e40275f18133446a74e1e2ea3408f511b3e89
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-11-15 13:27:41 +0530

    redicator

--- mongodb.sh
+++ mongodb.sh
@@ -4,9 +4,9 @@ cp /home/centos/prroboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/
 echo -e "\e33m Installing MongoDB Server \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m update MongoDB Listen Address \[0m"
+echo -e "\e[33m update MongoDB Listen Address \e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Start MongoDB Service \[0m"
+echo -e "\e[33m Start MongoDB Service \e[0m"
 systemctl enable mongod  &>>/tmp/roboshop.log
 systemctl restart mongod  &>>/tmp/roboshop.log
